e35b30b8177b6c52d1a5f46bb21a9a06dbeb23b4
vivekthangaswamy <vivekthangaswamy@gmail.com>
2017-08-24 15:53:29 +1000

    update

---
+++ SportsVaralaaruAPI/Migrations/20170824034034_dbseed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SportsVaralaaruAPI.Migrations
+{
+    public partial class dbseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Kind = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SportName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sports");
+        }
+    }
+}
